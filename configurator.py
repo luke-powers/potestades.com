a5bd61f1d350b79d8a59feab4664ce154681a748
Luke Powers <los.powers@gmail.com>
2024-09-17 11:45:44 -0700

    Add configurator to potestades project

---
+++ configurator.py
@@ -0,0 +1,38 @@
+import sys
+from pprint import pprint
+from urllib.parse import urljoin
+
+import requests
+
+if __name__ == "__main__":
+    helptxt = (
+        "> configurator.py <API_TOKEN> <USER> <WEB_PROJECT> <DOMAIN>\n"
+        "Note that web project must have a virtual env and a main.py associated with it"
+    )
+    if [y for y in sys.argv if "-h" in y]:
+        pprint(helptxt)
+        exit()
+    try:
+        _, api_token, user, web_project, domain_name = sys.argv
+    except ValueError:
+        print("need all values provided")
+        print(helptxt)
+        exit()
+    headers = {"Authorization": api_token}
+    api_base = f"https://www.pythonanywhere.com/api/v1/user/{user}/"
+    command = (
+        f"/home/{user}/.virtualenvs/{web_project}/bin/uvicorn "
+        "--uds $DOMAIN_SOCKET "
+        f"{web_project}.main:app"
+    )
+    response = requests.post(
+        urljoin(api_base, "websites/"),
+        headers=headers,
+        json={
+            "domain_name": domain_name,
+            "enabled": True,
+            "webapp": {"command": command},
+        },
+    )
+    pprint(response)
+    pprint(response.json())
