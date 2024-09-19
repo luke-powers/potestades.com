9869e7e9dac005f3ea774f8c30f96938e5068038
Luke Powers <los.powers@gmail.com>
2024-09-19 13:21:18 -0700

    Add http->https redirect

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(title="Potestades.com")
+app.add_middleware(HTTPSRedirectMiddleware)
 
 app.mount(
     "/", StaticFiles(directory="potestades/public", html=True), name="static_page"
