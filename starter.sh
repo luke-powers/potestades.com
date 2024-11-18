7fb2ef7236f6496429c85cac2fabeb0bd41ce004
Luke Powers <los.powers@gmail.com>
2024-11-18 13:15:16 -0800

    change port to 80 (default for uvicorn is 8000)

--- starter.sh
+++ starter.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source .venv/*/bin/activate
-uvicorn --ssl-keyfile=potestades.com.key --ssl-certfile=potestades.com.cert main:app --host 0.0.0.0
+uvicorn --ssl-keyfile=potestades.com.key --ssl-certfile=potestades.com.cert main:app --host 0.0.0.0 --port 80
