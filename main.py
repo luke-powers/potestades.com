f156c9813e5b191e8a9367b2dd992f36d43ba70d
Luke Powers <los.powers@gmail.com>
2026-02-15 19:21:49 -0800

    Attempt to merge shurlrtener with homepage code in a working fashion

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
-app = FastAPI(title="potestades.com")
-app.add_middleware(HTTPSRedirectMiddleware)
+from shurlrtener import shortener, setter, show, lengthener
+
+APP = FastAPI(title="potestades.com")
+APP.add_middleware(HTTPSRedirectMiddleware)
 
 
-@app.get("/favicon.ico", include_in_schema=False)
+@APP.get("/favicon.ico", include_in_schema=False)
 async def favicon():
-    return FileResponse("personal/static/img/favicon.ico")
+    return FileResponse("personal/favicon.ico")
 
 
-app.mount("/coach", StaticFiles(directory="coach/public", html=True), name="Coach Page")
+@APP.get("/shorten/{url:path}")
+async def sh(request: Request, url: str):
+    return await shortener(request, url)
 
-app.mount(
+
+@APP.get("/set/{code}/{url:path}")
+async def se(request: Request, code: str, url: str):
+    return await setter(request, code, url)
+
+
+@APP.get("/{code}")
+async def len(code: str):
+    return await lengthener(code)
+
+
+@APP.get("/show/")
+async def sho():
+    return await show()
+
+
+APP.mount("/coach", StaticFiles(directory="coach/public", html=True), name="Coach Page")
+
+APP.mount(
     "/", StaticFiles(directory="personal/public", html=True), name="Personal Page"
 )
